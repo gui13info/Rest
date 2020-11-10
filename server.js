05076b0b7eacdb8026c5f8cefd4c2d164d8e5567
Guilherme Matos <marques.guilherme0067@gmail.com>
2020-11-09 22:39:11 -0300

    Criando endpoints

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.get('/clients', function(req, res) {
+    
+});
+
+app.get('/clients/:id', function(req, res) {});
+app.post('/clients', function(req, res) {});
+app.put('/clients', function(req, res) {});
+app.put('/clients/:id', function(req, res) {});
+app.delete('/clients', function(req, res) {});
+app.delete('/clients/:id', function(req, res) {});
+
 app.listen(3000, function(){
     console.log('Server os running');
 });
