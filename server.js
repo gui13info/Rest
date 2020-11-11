01d1874f83bf7737382c2d4d526225b0715aaca3
Guilherme Matos <marques.guilherme0067@gmail.com>
2020-11-10 21:49:10 -0300

    Ajustando porta

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const data = require('./data.json');
+const port = 3000;
+
+app.use(express.json());
 
 app.get('/clients', function(req, res) {
-    
+    res.json(data);
 });
 
 app.get('/clients/:id', function(req, res) {});
@@ -12,6 +16,6 @@ app.put('/clients/:id', function(req, res) {});
 app.delete('/clients', function(req, res) {});
 app.delete('/clients/:id', function(req, res) {});
 
-app.listen(3000, function(){
-    console.log('Server os running');
+app.listen(port, function(){
+    console.log(`Server is running in port: ${port}`);
 });
