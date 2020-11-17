b226c52983062e9db8d76a8f39ae2c2f5ea2ef55
Guilherme Matos <marques.guilherme0067@gmail.com>
2020-11-17 17:31:00 -0300

    Finish

--- server.js
+++ server.js
@@ -9,12 +9,42 @@ app.get('/clients', function(req, res) {
     res.json(data);
 });
 
-app.get('/clients/:id', function(req, res) {});
-app.post('/clients', function(req, res) {});
-app.put('/clients', function(req, res) {});
-app.put('/clients/:id', function(req, res) {});
-app.delete('/clients', function(req, res) {});
-app.delete('/clients/:id', function(req, res) {});
+app.get("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+  
+    if (!client) return res.status(204).json();
+  
+    res.json(client);
+  });
+  
+  app.post("/clients", function(req, res) {
+    const { name, email } = req.body;
+  
+    // salvar
+  
+    res.json({ name, email });
+  });
+  
+  app.put("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+  
+    if (!client) return res.status(204).json();
+  
+    const { name } = req.body;
+  
+    client.name = name;
+  
+    res.json(client);
+  });
+  
+  app.delete("/clients/:id", function(req, res) {
+    const { id } = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+  
+    res.json(clientsFiltered);
+  });
 
 app.listen(port, function(){
     console.log(`Server is running in port: ${port}`);
